0ba875dcbf5deb5f6df6e2f0f3701f4e73b52cc3
JakeBoord <boord01@allegheny.edu>
2020-09-27 14:29:58 -0400

    Update __main__.py
    Added the necessary source code to main

--- converter/converter/__main__.py
+++ converter/converter/__main__.py
@@ -12,9 +12,12 @@ def main(
     temperature: float = typer.Option(98.6, min=-130, max=140),
 ):
     """Convert units.from Fahrenheit to Celsius or from Celsius to Fahrenheit."""
-    # TODO: display the two input parameters provided on the command line
-    # TODO: perform the conversion of the temperature from one unit to another unit
-    # TODO: display the original temperature and then the converted temperature, always using units
+    # display the two input parameters provided on the command line
+    typer.echo(f"Converting from {from_unit.value} to {to_unit.value}!")
+    # perform the conversion of the temperature from one unit to another unit
+    converted_temperature = convert.convert_temperature(temperature, from_unit, to_unit)
+    # display the original temperature and then the converted temperature, always using units
+    typer.echo(f"{temperature:.2f} degrees in {from_unit.value} is {converted_temperature:.2f} degrees in {to_unit.value}")
 
 
 if __name__ == "__main__":
